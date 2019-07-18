80304977f27d9580b7434eb0c7562ccb54686b11
ErikBoesen <me@erikboesen.com>
2019-07-17 20:49:36 -0400

    Steal MDN IDB code

--- search.js
+++ search.js
@@ -7,6 +7,61 @@ function getCookie(name) {
     return v ? v[2] : null;
 }
 const GROUPME_TOKEN = getCookie('token');
+log('token ' + GROUPME_TOKEN);
+
+(function () {
+  const DB_NAME = 'bgm';
+  const DB_VERSION = 1; // Use a long long for this value (don't use a float)
+  const DB_STORE_NAME = 'publications';
+
+  var db;
+
+  function openDb() {
+    console.log("openDb ...");
+    var req = indexedDB.open(DB_NAME, DB_VERSION);
+    req.onsuccess = function (evt) {
+      // Equal to: db = req.result;
+      db = this.result;
+      console.log("openDb DONE");
+    };
+    req.onerror = function (evt) {
+      console.error("openDb:", evt.target.errorCode);
+    };
+
+    req.onupgradeneeded = function (evt) {
+      console.log("openDb.onupgradeneeded");
+      var store = evt.currentTarget.result.createObjectStore(
+        DB_STORE_NAME, { keyPath: 'id', autoIncrement: true });
+
+      store.createIndex('biblioid', 'biblioid', { unique: true });
+      store.createIndex('title', 'title', { unique: false });
+      store.createIndex('year', 'year', { unique: false });
+    };
+  }
+
+  /**
+   * @param {string} store_name
+   * @param {string} mode either "readonly" or "readwrite"
+   */
+  function getObjectStore(store_name, mode) {
+    var tx = db.transaction(store_name, mode);
+    return tx.objectStore(store_name);
+  }
+
+  function getBlob(key, store, success_callback) {
+    var req = store.get(key);
+    req.onsuccess = function(evt) {
+      var value = evt.target.result;
+      if (value)
+        success_callback(value.blob);
+    };
+  }
+
+  openDb();
+
+
+
+})();
 
 function index(id) {
     log('indexing ' + id);
@@ -46,8 +101,8 @@ ready('.composer-wrapper', function(element) {
     let searchBox = document.createElement('div');
     searchBox.id = 'search-composer';
     // TODO: build this with DOM manipulation
-    searchBox.innerHTML = '<div class="composer-wrapper"><div role="textbox" class="emoji-wysiwyg-editor" contenteditable="true" placeholder="Search..."></div></div>';
-    searchBox.innerHTML += '<div class="controls controls-left"><button type="button" class=""><i class="gmn-icon-search"></i></button></div>';
+    searchBox.innerHTML = '<div class='composer-wrapper'><div role='textbox' class='emoji-wysiwyg-editor' contenteditable='true' placeholder='Search...'></div></div>';
+    searchBox.innerHTML += '<div class='controls controls-left'><button type='button' class=''><i class='gmn-icon-search'></i></button></div>';
     let composer = chat.getElementsByClassName('composer-wrapper')[0];
     composer.parentNode.insertBefore(searchBox, composer);
 });
